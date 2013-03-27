1a2aa553b234ceb83a2f6476292c68d7a20ecd4d
Gian Carlo Pace <giancarlo.pace@gmail.com>
2013-03-27 16:04:42 +0100

    first form saving

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,14 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+      redirect_to home_url
+    else
+      render :new
+    end
+  end
+
 end
