d3b9811f4e0adad8765dababa66ff8af5ac0c0c6
Gian Carlo Pace <giancarlo.pace@gmail.com>
2013-03-28 09:51:41 +0100

    Cleaned up

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,14 +1,24 @@
 class CustomersController < ApplicationController
+  respond_to :html, :js, :json
+
   def new
     @customer = Customer.new
   end
 
   def create
     @customer = Customer.new(params[:customer])
-    if @customer.save
-      redirect_to home_url
-    else
-      render :new
+    respond_with do |format|
+      if @customer.save
+        redirect_to home_url
+      else
+        format.js { render :json => { :error => @customer.errors.full_messages.to_sentence },
+                           :status => :unprocessable_entity
+        }
+        format.json { render :json => { :error => @customer.errors.full_messages.to_sentence },
+                           :status => :unprocessable_entity
+        }
+        format.html { render :new }
+      end
     end
   end
 
